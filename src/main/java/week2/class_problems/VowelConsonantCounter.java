32ffbdc39933e207ac277572dbebfc2144771bad
Yash7548-SRM <ya7548@srmist.edu.in>
2026-09-04 22:30:04 +0530

    Week 2 class problems

---
+++ src/main/java/week2/class_problems/VowelConsonantCounter.java
@@ -0,0 +1,24 @@
+package week2.class_problems;
+
+public class VowelConsonantCounter {
+
+    static void countVowelsAndConsonants(String text) {
+        int vowels = 0, consonants = 0;
+
+        for (int i = 0; i < text.length(); i++) {
+            char c = Character.toLowerCase(text.charAt(i));
+            if (c == ' ') continue;
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                vowels++;
+            } else if (c >= 'a' && c <= 'z') {
+                consonants++;
+            }
+        }
+
+        System.out.println("Vowels: " + vowels + " | Consonants: " + consonants);
+    }
+
+    public static void main(String[] args) {
+        countVowelsAndConsonants("Java Programming");
+    }
+}
